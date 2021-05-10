52962bc11d73c9d0afc3c16b647a8d26be59c3ea
Doderovic Milan <mdoderovic5@gmail.com>
2021-05-10 22:03:00 +0200

    Count Nodes

--- Solving Binary Tree Problems/src/solving/binary/tree/problems/SolvingBinaryTreeProblems.java
+++ Solving Binary Tree Problems/src/solving/binary/tree/problems/SolvingBinaryTreeProblems.java
@@ -16,6 +16,39 @@ public class SolvingBinaryTreeProblems {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Node<String> a = new Node<>("Alice");
+        Node<String> b = new Node<>("Bob");
+        Node<String> c = new Node<>("Charles");
+        Node<String> d = new Node<>("Denis");
+        Node<String> e = new Node<>("Elsa");
+        Node<String> f = new Node<>("Figo");
+        Node<String> g = new Node<>("Gabriel");
+        Node<String> h = new Node<>("Hugo");
+        Node<String> i = new Node<>("Irina"); 
+        
+        a.setLeftChild(b);
+        a.setRightChild(c);
+        
+        b.setLeftChild(d);
+        c.setLeftChild(e);
+        c.setRightChild(f);
+        f.setLeftChild(g);
+        f.setRightChild(h);
+        h.setRightChild(i);
+        
+        System.out.println("Nodes of root a : "+countNodes(a));
+        System.out.println("Nodes of root c : "+countNodes(c));
+
+    }
+    
+    public static <T> int countNodes(Node<T> root){
+        if(root==null)
+            return 0;
+
+        int leftNodes = countNodes(root.getLeftChild());
+        int rightNodes =  countNodes(root.getRightChild());    
+        
+        return 1+leftNodes + rightNodes;
     }
     
 }
